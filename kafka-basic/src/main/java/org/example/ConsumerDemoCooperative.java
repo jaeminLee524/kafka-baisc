92299677535bae3b108432ebfbe294afcc3922d5
이재민 <rkwoals524@hanmail.net>
2024-02-24 19:41:38 +0900

    CooperativeStickyAssioner strategy

---
+++ kafka-basic/src/main/java/org/example/ConsumerDemoCooperative.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsumerDemoCooperative {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoCooperative.class);
+
+    public static void main(String[] args) {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-fourth-application";
+        String topic = "demo_java";
+
+        // consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer .class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
+
+        // create kafka consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // get main Thread
+        final Thread mainThread = Thread.currentThread();
+
+        // add shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                log.info("Detected a shutdown.");
+                // consumer.poll() 메소드가 수행되면, 알아서 wakeup 예외를 던집니다.
+                consumer.wakeup();
+
+                // main Thread에 합류해서 메인 스레드 코드 실행을 허용합니다.
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        try {
+            // subscribe consumer
+            consumer.subscribe(Arrays.asList(topic));
+
+            // poll for new data
+            while (true) {
+                // 카프카에 데이터가 없을 경우 1초를 기다립니다.
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key: " + record.key() + ", Value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+                }
+            }
+        } catch (WakeupException ex) {
+            log.info("Consumer is starting to shut down.");
+        } catch (Exception ex) {
+            log.error("Unexpected exception in the consumer.");
+        } finally {
+            // consumer 종료, 오프셋 커밋, 컨슈머 그룹도 리밸런싱
+            consumer.close();
+            log.info("The consumer is now gracefully shut down.");
+        }
+    }
+
+}
